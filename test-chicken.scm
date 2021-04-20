b065278d50d5fd23490f6eeb9e1035d2e8fc9508
Wolfgang Corcoran-Mathe <wcm@sigwinch.xyz>
2021-04-20 19:39:03 -0400

    Add promised CHICKEN test shim.

---
+++ test-chicken.scm
@@ -0,0 +1,23 @@
+;;; Test framework using CHICKEN's test egg.
+(import (srfi 1)
+        (srfi 128)
+        (srfi 158)
+        (srfi 189)
+        (srfi 224)
+        (test))
+
+;; SRFI 64 shim.
+
+(define-syntax test-eqv
+  (syntax-rules ()
+    ((test-eqv expected expr)
+     (test expected expr))))
+
+(define-syntax test-equal
+  (syntax-rules ()
+    ((test-equal expected expr)
+     (test expected expr))))
+
+(include "test-body.scm")
+
+(test-exit)
