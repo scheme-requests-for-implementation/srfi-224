scheme-requests-for-implementation/srfi-224

commit ad299d97b75a412e2893607b8cc43e105d0ba23b

Avoid unneeded arglist allocation.

srfi/224.scm
@@ -33,7 +33,7 @@
 (define (second-arg _ y) y)
 
 (define (constantly x)
-  (lambda _ x))
+  (lambda (_) x))
 
 ;;;; Type
 

commit 877b955b7178bc7fb86ec0dd23057640b67ab3d1

Fix arity bug introduced by ad299d97.

srfi/224.scm
@@ -266,7 +266,7 @@
 ;; Delete the element with the least key, or return an empty
 ;; mapping if `imap' is empty.
 (define (imapping-delete-min imap)
-  (imapping-update-min/key imap (constantly (nothing))))
+  (imapping-update-min imap (constantly (nothing))))
 
 (define (imapping-update-min imap success)
   (%imapping-update-min imap success #f))
@@ -296,7 +296,7 @@
 ;; Delete the element with the greatest key, or return an empty
 ;; mapping if `imap' is empty.
 (define (imapping-delete-max imap)
-  (imapping-update-max/key imap (constantly (nothing))))
+  (imapping-update-max imap (constantly (nothing))))
 
 (define (imapping-update-max imap success)
   (%imapping-update-max imap success #f))